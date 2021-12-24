7ead6b937989448f5b379218193c9a1ed20accf9
tekihei2317 <tekihei2317@gmail.com>
2021-12-24 18:08:07 +0900

    feat: ec2インスタンス用の設定を作成
    TODO:
    - セッションマネージャを使えるようにする
    - パラメータストアの情報をEC2内のファイルに保存する

---
+++ ec2/main.tf
@@ -0,0 +1,82 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block           = "10.0.0.0/21"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = { Name = "tf01" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+
+  tags = { Name = "tf01-public-subnet" }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = { Name = "tf01-gateway" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  tags = { Name = "tf01-route-table" }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "example" {
+  name   = "tf01-security-group"
+  vpc_id = aws_vpc.example.id
+
+  tags = { Name = "tf01-security-group" }
+}
+
+resource "aws_security_group_rule" "ingress" {
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0218d08a1f9dac831"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.example.id]
+
+  user_data = <<EOF
+    #! /bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+  EOF
+
+  tags = { Name = "tf01-instance" }
+}
